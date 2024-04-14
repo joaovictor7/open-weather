42e754d7f3051c4353b171b95eb0707d3660f8eb
joao.matos <joaovictormatos705@gmail.com>
2024-04-14 15:55:14 +0100

    warning hilt inject context resolved

--- app/src/main/java/com/composetest/application/MainApplication.kt
+++ app/src/main/java/com/composetest/application/MainApplication.kt
@@ -1,7 +1,14 @@
 package com.composetest.application
 
 import android.app.Application
+import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
 
 @HiltAndroidApp
-class MainApplication: Application()
+class MainApplication : Application() {
+    @Inject
+    @ApplicationContext
+    lateinit var context: Context
+}

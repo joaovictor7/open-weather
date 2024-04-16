4a5d105e12b4bfe56b4e55ccd451614997976098
joao.matos <joaovictormatos705@gmail.com>
2024-04-16 15:28:08 +0100

    removing nav controller hilt component

--- router/src/main/java/com/composetest/router/managers/NavControllerManager.kt
+++ router/src/main/java/com/composetest/router/managers/NavControllerManager.kt
@@ -1,25 +1,16 @@
 package com.composetest.router.managers
 
 import androidx.navigation.NavHostController
-import com.composetest.router.managers.components.NavControllerComponentBuilder
-import com.composetest.router.managers.components.NavHostControllerEntryPoint
-import dagger.hilt.EntryPoints
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class NavControllerManager @Inject constructor(
-    private val builder: NavControllerComponentBuilder
-) {
+class NavControllerManager @Inject constructor() {
 
     var navController: NavHostController? = null
         private set
 
     fun setNavController(navController: NavHostController) {
-        val component = builder.setNavController(navController).build()
-        this.navController = EntryPoints.get(
-            component,
-            NavHostControllerEntryPoint::class.java
-        ).navController
+        this.navController = navController
     }
 }
